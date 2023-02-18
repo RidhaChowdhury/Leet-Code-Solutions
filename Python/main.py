71514e2d3d047d8541c0a7f7547e5b06dd3e6c24
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-02-18 15:54:50 -0500

    binary search, with uhh, binary search

--- Python/main.py
+++ Python/main.py
@@ -820,3 +820,16 @@ def mySqrt(self, x: int) -> int:
             else: 
                 return middle
         return closest
+
+def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        middle = (left + right) // 2
+        while left<=right:
+            if nums[middle] < target:
+                left = middle + 1
+            elif nums[middle] > target:
+                right = middle - 1
+            else:
+                return middle
+            middle = (left + right) // 2
+        return -1
