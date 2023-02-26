e533f13da2171b42cfb4b4d0408da967ad6f8abc
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-02-25 21:10:19 -0500

    maxDepth recursive

--- Python/main.py
+++ Python/main.py
@@ -930,3 +930,9 @@ def findMin(self, nums: List[int]) -> int:
                 right = middle
 
         return nums[left]
+
+def maxDepth(self, root: Optional[TreeNode]) -> int:
+    if root == None:
+        return 0
+    
+    return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
